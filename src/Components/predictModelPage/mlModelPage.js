bf28560d8ee47bbaed3778fd06c815d8c27e4475
Mateusz Ostrowski <34621910+MattyOstrowsky@users.noreply.github.com>
2022-03-02 00:31:34 +0100

    update ML only

--- src/Components/predictModelPage/mlModelPage.js
+++ src/Components/predictModelPage/mlModelPage.js
@@ -1,26 +1,50 @@
-import React from 'react';
-
-import Paper from '@material-ui/core/Paper';
-import ModelForm from './modelForm'
-
-
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import ModelForm from "./modelForm";
 
 export default function MlModelPage() {
-    return (
-
-        <Paper elevation={3}
-            style={{
-                display: 'flex',
-                margin: 'auto',
-                marginTop: '8rem',
-                width: '80vw',
-                paddingBottom: '2rem',
-                borderRadius: '25px',
-                marginBottom: '10vw',
-            }}>
-            <ModelForm />
-        </Paper>
-
-    )
+  return (
+    <div
+      style={{
+        paddingTop: "10vw",
+        paddingBottom: "10vw",
+      }}
+    >
+      <Paper
+        elevation={3}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "auto",
+          width: "80vw",
+          paddingBottom: "2rem",
+          borderRadius: "25px",
+        }}
+      >
+        <p
+          style={{
+            padding: "3rem 3rem 0rem 3rem",
+          }}
+        >
+          A stroke is a life-threatening condition in which part of the brain
+          dies as a result of a stoppage of blood supply to its tissues. The
+          specific symptoms of a stroke occur suddenly and give little time for
+          emergency action. On the basis of collected data, a machine learning
+          model has been created, which is able to predict on the basis of given
+          data with what probability a stroke is possible.
+        </p>
+        <p
+          style={{
+            padding: "2rem",
+            color: "#b53026",
+            textAlign: "center",
+          }}
+        >
+          Remember! The result is not binding. To confirm your suspicions please
+          contact your doctor!
+        </p>
+        <ModelForm />
+      </Paper>
+    </div>
+  );
 }
-
--- src/Components/predictModelPage/modelButton.js
+++ src/Components/predictModelPage/modelButton.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import { Button as MuiButton } from '@material-ui/core';
-
-
+import React from "react";
+import { Button as MuiButton } from "@material-ui/core";
 
 export default function ModelButton(props) {
+  const { text, size, color, variant, onClick, ...other } = props;
 
-    const { text, size, color, variant, onClick, ...other } = props
-
-    return (
-        <MuiButton
-            variant={variant}
-            size={size}
-            color={color}
-            onClick={onClick}
-            style={{ width: '30vw', margin: '3rem' }}
-            {...other}>
-            {text}
-        </MuiButton>
-    )
+  return (
+    <MuiButton
+      variant={variant}
+      size={size}
+      color={color}
+      onClick={onClick}
+      style={{ width: "30vw", margin: "3rem" }}
+      {...other}
+    >
+      {text}
+    </MuiButton>
+  );
 }
